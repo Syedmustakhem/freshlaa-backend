ecf52bb293fd11257ac753b2f412117092497711
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2025-12-30 17:33:17 +0530

    fixed address

--- src/routes/address.routes.js
+++ src/routes/address.routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middlewares/auth.middleware");
 const {
-  addAddress,
   getAddresses,
+  addAddress,
+  updateAddress,
   deleteAddress,
 } = require("../controllers/address.controller");
 
-/* Protected routes */
-router.post("/", auth, addAddress);
-router.get("/", auth, getAddresses);
-router.delete("/:id", auth, deleteAddress);
+const { protect } = require("../middlewares/auth.middleware");
+
+/* ROUTES */
+router.get("/", protect, getAddresses);
+router.post("/", protect, addAddress);
+router.put("/:id", protect, updateAddress);
+router.delete("/:id", protect, deleteAddress);
 
 module.exports = router;
