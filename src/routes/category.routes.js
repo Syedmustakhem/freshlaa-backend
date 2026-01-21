46d086cb19331d049b050e5caf7d99651ede6c3c
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2026-01-21 21:57:50 +0530

    category

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const { getCategories } = require("../controllers/category.controller");
+const Category = require("../models/Category");
 
-router.get("/", getCategories);
+// GET categories (app + admin)
+router.get("/", async (req, res) => {
+  const categories = await Category.find({ isActive: true });
+  res.json(categories);
+});
+
+// POST category (admin)
+router.post("/", async (req, res) => {
+  try {
+    const { name, slug, images, isActive } = req.body;
+
+    const category = await Category.create({
+      name,
+      slug,
+      images,
+      isActive,
+    });
+
+    res.status(201).json(category);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to add category" });
+  }
+});
 
 module.exports = router;
