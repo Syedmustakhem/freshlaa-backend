96ce95303af7b8667ae9f374f6c1494cf7072980
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2026-01-26 17:26:29 +0530

    fixed categories

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use("/api/hotel/menu", hotelMenuRoutes);
 app.use("/api/payment-methods", paymentMethodRoutes);
 app.use("/api/razorpay", razorpayRoutes);
 app.use("/api/restaurants", restaurantRoutes);
-app.use("/api/category", categoryRoutes);
+app.use("/api/categories", categoryRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/banners", bannerRoutes);
 app.use("/api/admin/push", adminPushRoutes);
