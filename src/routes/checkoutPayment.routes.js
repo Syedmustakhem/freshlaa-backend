59a0fae7435fdde35bf08a8a9f24412f68cc503f
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2026-02-21 15:15:37 +0530

    fixed

--- src/routes/checkoutPayment.routes.js
+++ src/routes/checkoutPayment.routes.js
@@ -1,27 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const protect  = require("../middlewares/auth.middleware");
-const checkoutPaymentService = require("../services/checkoutPayment.service");
 
-router.get("/config", protect, async (req, res) => {
-  try {
-    const amount = Number(req.query.amount || 0);
+const protect = require("../middlewares/auth.middleware");
+const controller = require("../controllers/checkoutPayment.controller");
 
-    const config = await checkoutPaymentService.getCheckoutConfig({
-      user: req.user,
-      amount,
-    });
-
-    res.json({
-      success: true,
-      methods: config,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error.message,
-    });
-  }
-});
+router.get("/config", protect, controller.getCheckoutConfig);
 
 module.exports = router;
