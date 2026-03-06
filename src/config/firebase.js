a1c899afbee6ada4faa170be1d90b38f1835da68
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2026-03-07 00:32:28 +0530

    fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-
+const serviceAccount = require("./freshlaa-firebase-adminsdk-fbsvc-08331eee7d.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
