44c371a47b5718ed98ed863aa9748cf6c4b9e71d
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2026-03-08 13:20:39 +0530

    fiexed

--- src/routes/appConfig.routes.js
+++ src/routes/appConfig.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Update these values whenever you release a breaking update
 const APP_CONFIG = {
-  min_version_android: "1.0.0",  // ← bump this to force update
+  min_version_android: "1.1.0",  // ← bump this to force update
   min_version_ios: "1.0.0",      // ← bump this for iOS
   message: "A new version of FreshLaa is available with important improvements.",
 };
