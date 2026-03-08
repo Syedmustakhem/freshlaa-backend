fb10d452f55ce2c1cd926b8070980bfa9a31ac19
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2026-03-08 16:11:22 +0530

    fixed

--- src/routes/appConfig.routes.js
+++ src/routes/appConfig.routes.js
@@ -2,17 +2,22 @@
 const express = require("express");
 const router = express.Router();
 
-// Update these values whenever you release a breaking update
 const APP_CONFIG = {
-  min_version_android: "1.1.0",  // ← bump this to force update
-  min_version_ios: "1.0.0",      // ← bump this for iOS
-  message: "A new version of FreshLaa is available with important improvements.",
+  min_version_android: "1.1.0",
+  min_version_ios: "1.0.0",
+
+  force_update_message:
+    "A new version of FreshLaa is available with important improvements.",
+
+  maintenance_mode: false,
+  maintenance_message:
+    "FreshLaa is currently under maintenance. Please try again later."
 };
 
 router.get("/app-config", (req, res) => {
   res.json({
     success: true,
-    ...APP_CONFIG,
+    ...APP_CONFIG
   });
 });
 
